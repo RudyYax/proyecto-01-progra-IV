d9bbd22d37cffff80a2697c677e77e346e18d896
Rudy <rudyax2004@gmail.com>
2025-08-17 01:23:56 -0600

    commit rama rudy

--- proyecto.py
+++ proyecto.py
@@ -15,12 +15,7 @@ class Registrar_Producto:
     def agregar_producto(self):
         while True:
             try:
-                codigo = input("Ingresa el codigo del producto (ENTER para volver al menú de categorías, 0 para regresar al menú principal): ")
-                if codigo == "":
-                    break
-                if codigo == "0":
-                    return
-                codigo = int(codigo)
+                codigo = int(input("Ingresa el codigo del producto: "))
                 if codigo in self.producto:
                     print("El codigo del producto ya existe.")
                     error = input("Presione ENTER para ingresar nuevamente o 0 para salir:")
@@ -33,6 +28,11 @@ class Registrar_Producto:
                 stock_producto = input("Ingresa el stock del producto: ")
                 self.producto[codigo] = Ingreso(codigo, nombre_producto, precio_producto, stock_producto)
                 print("Producto registrado correctamente.")
+                intento = input("Presione ENTER para ingresar otro producto o ingrese 0 para registrar otra categoria")
+                if intento == "0":
+                    break
+                else:
+                    continue
             except ValueError:
                 print("No se puedo agregar un producto")
                 continue
@@ -51,7 +51,7 @@ class Registrar_Producto:
 registro_Codigo = Registrar_Producto()
 
 opcion = 0
-while opcion != 5:
+while True:
     print("Bienvenidos a la tienda de Ropa ")
     print("1.- Registrar Producto")
     print("2.- Mostrar Productos")
@@ -59,41 +59,46 @@ while opcion != 5:
     print("4.- Gestion de Productos")
     print("5.- Salir")
     opcion = int(input("seleccione una de las opciones que desee: "))
+    while True:
+        match opcion:
+            case 1:
+                print("Registrar Producto")
+                print("A continuacion se le presenta las Categorias disponibles")
+                print("1.- Playeras")
+                print("2.- Pantalones")
+                print("3.- Sueteres")
+                print("4.- Zapatos")
+                categoria = int(input("seleccione una de las opciones o presione 0 para regresar al menú principal: "))
+                match categoria:
+                    case 0:
+                        break
+                    case 1:
+                        while True:
+                            print("Categoria Playeras")
+                            registro_Codigo.agregar_producto()
+                            break
 
-    match opcion:
-        case 1:
-            print("Registrar Producto")
-            print("A continuacion se le presenta las Categorias disponibles")
-            print("1.- Playeras")
-            print("2.- Pantalones")
-            print("3.- Sueteres")
-            print("4.- Zapatos")
-            categoria = int(input("seleccione una de las opciones: "))
-            match categoria:
-                case 1:
-                    while True:
-                        print("Categoria Playeras")
+
+
+                    case 2:
+                        print("Categoria Pantalones")
+                        registro_Codigo.agregar_producto()
+                    case 3:
+                        print("Categoria Sueteres")
+                        registro_Codigo.agregar_producto()
+                    case 4:
+                        print("Categoria Zapatos")
                         registro_Codigo.agregar_producto()
 
-                case 2:
-                    print("Categoria Pantalones")
-                    registro_Codigo.agregar_producto()
-                case 3:
-                    print("Categoria Sueteres")
-                    registro_Codigo.agregar_producto()
-                case 4:
-                    print("Categoria Zapatos")
-                    registro_Codigo.agregar_producto()
 
 
-
-        case 2:
-            print("Mostrar Productos")
-            registro_Codigo.Mostrar_Productos()
-
-        case 3:
-            print("Buscar Producto")
-        case 4:
-            print("Gestion de Productos ")
-        case 5:
-            print("Salir")
+            case 2:
+                print("Mostrar Productos")
+                registro_Codigo.Mostrar_Productos()
+                break
+            case 3:
+                print("Buscar Producto")
+            case 4:
+                print("Gestion de Productos ")
+            case 5:
+                print("Salir")
